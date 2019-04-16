3a5737e0db49516ea89f3e8d9b355545722ec7f2
edwinlaurabegazo <49194164+edwinlaurabegazo@users.noreply.github.com>
2019-04-16 12:01:09 -0500

    Add files via upload

---
+++ While.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgwhile;
+
+/**
+ *
+ * @author PC05-LAB02
+ */
+public class While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=1;
+        while(a<=10)
+        {
+            System.out.println(a);
+            a=a+1;
+            
+        }
+    }
+    
+}
