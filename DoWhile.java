1e0c58285a7faf42c21e4259d10b5984b3edb5ea
edwinlaurabegazo <49194164+edwinlaurabegazo@users.noreply.github.com>
2019-04-16 12:02:59 -0500

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgdo.pkgwhile;
+
+/**
+ *
+ * @author PC05-LAB02
+ */
+public class DoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=1;
+        do
+        {
+            System.out.println(a);
+            a=a+1;
+        }while(a<=100);
+    }
+    
+}
